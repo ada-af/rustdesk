e10b9fdaa7c30fea980cdc52a101ab4c0d71ef16
Luna <ilya.tretyakov85@gmail.com>
2022-11-30 16:17:15 +1000

    user toString()

--- flutter/lib/common/widgets/peer_card.dart
+++ flutter/lib/common/widgets/peer_card.dart
@@ -605,7 +605,7 @@ abstract class BasePeerCard extends StatelessWidget {
         await bind.mainSetPeerAlias(id: id, alias: name);
         if (isAddressBook) {
           gFFI.abModel.setPeerAlias(id, name);
-          await gFFI.abModel.pushPeerAlias(id, name);
+          await gFFI.abModel.pushPeerAlias(id, name.toString());
         }
         if (isAddressBook) {
           gFFI.abModel.pullAb();
